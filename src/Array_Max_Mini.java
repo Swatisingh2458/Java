d6198015ed9cfb61617f821d9613bc9ca9e16099
swati2458 <swatisingh245899@gmail.com>
2024-10-23 23:52:42 +0530

    Array

--- src/Array_Max_Mini.java
+++ src/Array_Max_Mini.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Array_Max_Mini {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the Size:");
-        int size = input.nextInt();
-        int [] Arr = new int[size];
-        for(int i =0; i<size; i++){
-            
+
+
+        int[] Arr = {3,5,8,9,1};
+        int Min = Integer.MIN_VALUE;
+        int Max = Integer.MAX_VALUE;
+        for (int i = 0; i < Arr.length; i++) {
+            if (Arr[i] > Min) {
+                  Min= Arr[i];
+            } else {
+                if (Arr[i] < Max) {
+                    Max = Arr[i];
+                }
+            }
+
         }
+        System.out.println(Min);
+        System.out.println(Max);
     }
 }
